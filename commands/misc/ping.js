4ac3d7be0f8163da99b041b5208df89788ae1835
NPHCrafterDEV <75496294+NPHCrafterDEV@users.noreply.github.com>
2021-05-21 12:27:41 +0200

    Update ping.js

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: "ping",//default command name
   aliases: [],//other command names, the bot reacts to
-  usage: "!ping",//how to use the command
-  description: "pong",//description of the command
+  usage: "",//how to use the command
+  description: "",//description of the command
   run: async (client, message, args) => {
    message.channel.send("The Bot's ping is: " + client.ws.ping + "ms")
   }
